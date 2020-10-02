df89a97fb42c9cad4f0bab4621baa57793deb32d
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-10-02 18:35:48 +0530

    server is started....

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ const net = require('net') // step 1
 
 const server = net.createServer() // step 2
 
-const app = {}
+const app = {
+  listen: (port) => {
+    server.listen(port, ()=> {
+      console.log(`Server Started on`, server.address()) // step 3
+    })
+  }
+}
 module.exports = {app}
