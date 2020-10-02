154b157d4c3b9f13f8f535732939e1768f3a99da
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-10-02 21:19:35 +0530

    csv file is downloaded and Content-Type is added

--- http/csv.js
+++ http/csv.js
@@ -2,7 +2,12 @@ const http = require('http')
 const host = 'localhost'
 const port = 8000
 
-const requestListner = function (req, res) {}
+const requestListner = function (req, res) {
+  res.setHeader('Content-Type', 'text/csv')
+  res.setHeader('Content-Disposition', "attachment;filename=oceanpals.csv")
+  res.writeHead(200)
+  res.end(`id,name,email\n1,Sammy,Shark,Shark@ocean.com`)
+}
 
 const server = http.createServer(requestListner)
 server.listen(port, host, () => {
