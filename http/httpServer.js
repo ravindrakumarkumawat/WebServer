8d9e5f862a974b4f8a8e6c886118cabed14f5d53
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-10-03 10:21:45 +0530

    function name is changed

--- http/httpServer.js
+++ http/httpServer.js
@@ -2,12 +2,12 @@ const http = require('http')
 const host = 'localhost'
 const port = 8000
 
-const requestListner = function (req, res) {
+const requestListener = function (req, res) {
   res.writeHead(200)
   res.end('My first http server')
 }
 
-const server = http.createServer(requestListner)
+const server = http.createServer(requestListener)
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`)
 })
--- http/json.js
+++ http/json.js
@@ -2,13 +2,13 @@ const http = require('http')
 const host = 'localhost'
 const port = 8000
 
-const requestListner = function (req, res) {
+const requestListener = function (req, res) {
   res.setHeader('Content-Type', 'application/json')
   res.writeHead(200)
   res.end(`{"message": "This is a JSON response"}`)
 }
 
-const server = http.createServer(requestListner)
+const server = http.createServer(requestListener)
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`)
 })
