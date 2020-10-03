a7b64c6df3d0299c58d97340d35828564add1ab3
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-10-03 18:39:16 +0530

    function is created

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const net = require('net') // step 1
 const server = net.createServer() // step 2
 const host = 'localhost'
 
+const createServer = (port) => {
+  server.listen(port, ()=> {
+    console.log(`Server is running on http://${host}:${port}`)
+  })
+}
+
 const app = {
   listen: (port) => {
-    server.listen(port, ()=> {
-      console.log(`Server is running on http://${host}:${port}`)
-    })
+    createServer(port)
   }
 }
 module.exports = {app}
