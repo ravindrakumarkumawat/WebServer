376386dabf1071e82d8a38cb0032983ebac72048
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-10-03 16:09:04 +0530

    server host is changed

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const net = require('net') // step 1
 
 const server = net.createServer() // step 2
+const host = 'localhost'
 
 const app = {
   listen: (port) => {
     server.listen(port, ()=> {
-      console.log(`Server Started on -->`, server.address()) // step 3
-      console.log(`\n\nServer is running on http://localhost:${port}`)
+      console.log(`Server is running on http://${host}:${port}`)
     })
   }
 }
